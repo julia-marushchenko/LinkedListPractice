3b80246a3f041229e0f256509793ab9d32387e93
Julia Marushchenko <marushchenko.yulia@gmail.com>
2024-07-21 18:26:02 +0200

    Update LinkedListPractice.java
    Added Conparator for sorting list.

--- src/com/lists/main/LinkedListPractice.java
+++ src/com/lists/main/LinkedListPractice.java
@@ -1,6 +1,7 @@
 package com.lists.main;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class LinkedListPractice {
@@ -24,10 +25,10 @@ public class LinkedListPractice {
 		System.out.println(list.toString());
 		
 		// Getting the first element
-		System.out.println(list.peekFirst());
+		System.out.println("The first element: " + list.peekFirst());
 		
 		// Getting the last element
-		System.out.println(list.peekLast());
+		System.out.println("The last element: " + list.peekLast());
 		
 		// Index of first Blue, return index of the first found from the beginning
 		System.out.println("Index of first met colour Blue is:" + list.indexOf("Blue")); //0
@@ -36,34 +37,46 @@ public class LinkedListPractice {
 		System.out.println(list.pollFirst());
 		
 		// Showing all elements of the list, without the first one Blue
-		System.out.println(list.toString());
+		System.out.println("List after poll() method " + list.toString());
 		
 		// Index of first Blue, return index of the first found from the beginning
 		System.out.println("Index of first met colour Blue is:" + list.indexOf("Blue")); //4
 		
 		//Iterate all elements of the list
 		for (String element : list) {
-		System.out.println(element);
-		}
-		
-		//Iterate all elements of the list
-		for (String element : list) {
-			System.out.println(list.peek()); // Works not appropriate, shows only the first element
+			System.out.println(element);
 		}
 		
 		// Printing to console true or false if the list is empty
 		System.out.println("Is list empty: " + list.isEmpty());
 		
 		// Printing to console the size of the list
-		System.out.println(list.size());
+		System.out.println("Size of the is: " + list.size());
 		
 		// Sorting list in alphabetical order
 		Collections.sort(list);
 		
 		// Showing all elements of the list
-		System.out.println(list.toString());
+		System.out.println("All elements after sorting " + list.toString());
 		
+		Comparator<String> comparator = new Comparator<String>(){
+			
+			@Override
+			public int compare(String str, String str1) {
+				if (str.length() > str1.length()) {
+						return 1;
+				} else {
+					return 0;
+				}
+			}		
+		};
 		
+		// Sorting list in alphabetical order and with length descending order
+		Collections.sort(list, comparator);
 		
+		// Printing list
+		System.out.println("After sorting with comparator: " + list);	
+		
+			
 	}
 }
